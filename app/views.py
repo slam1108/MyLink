b6d48d2b9045614caec06efd10a4459c4e369734
yhong <yhong@purdue.edu>
2015-04-23 01:28:02 -0400

    base.html, index.html done.

--- app/views.py
+++ app/views.py
@@ -1,11 +1,27 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
+# route for handling index page
 @app.route('/')
-@app.route('/index')
-def index():
-	return "Hello, World!"
-
 @app.route('/welcome')
 def welcome():
 	return render_template('welcome.html')
+
+@app.route('/index')
+def index():
+	user = {'nickname': 'Yongsun'} # fake user
+	posts = [
+		{
+			'user': {'nickname': 'John'},
+			'content': 'Post of John!'
+		},
+		{
+			'user': {'nickname': 'Susan'},
+			'content': 'Post of Susan!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
+# route for handling welcome page
+
+
