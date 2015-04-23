f5ff4ac124d45878bec23e2af26ac4c8844c10c2
yhong <yhong@purdue.edu>
2015-04-23 02:21:59 -0400

    Web Forms
    forms.py
    config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+#Tell Flask to read it and use it.
+app.config.from_object('config')
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,12 +1,15 @@
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, flash
+from .forms import LoginForm
 
 # route for handling index page
 @app.route('/')
 @app.route('/welcome')
+# route for handling welcome page
 def welcome():
 	return render_template('welcome.html')
 
+# route for handling index page
 @app.route('/index')
 def index():
 	user = {'nickname': 'Yongsun'} # fake user
@@ -22,6 +25,15 @@ def index():
 	]
 	return render_template('index.html', title='Home', user=user, posts=posts)
 
-# route for handling welcome page
+# route for handling login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
+
+
 
 
